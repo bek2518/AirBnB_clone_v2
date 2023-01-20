82d23f46ab74d255a32870051d95e335444de8b9
bek2518 <bereketb20@hotmail.com>
2023-01-20 14:14:41 +0300

    Added getter

--- models/state.py
+++ models/state.py
@@ -14,3 +14,12 @@ class State(BaseModel, Base):
         cities = relationship('City', backref='state')
     else:
         name = ""
+
+        @property
+        def cities(self):
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
