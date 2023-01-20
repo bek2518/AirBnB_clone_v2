98ff38a542ad49be909dad190daed6de39365716
bek2518 <bereketb20@hotmail.com>
2023-01-20 11:51:29 +0300

    Trial

--- models/user.py
+++ models/user.py
@@ -13,12 +13,11 @@ class User(BaseModel, Base):
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
     else:
         email = ''
         password = ''
         first_name = ''
         last_name = ''
-
