7f8c105f7c4d5fd2f509a609711c4e42d4174a67
bek2518 <bereketb20@hotmail.com>
2023-01-20 12:30:36 +0300

    Places edited

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -15,6 +16,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user')
 
     else:
         email = ''
