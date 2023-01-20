d74599c44f7351894150319dec1f10e1d4b909cc
bek2518 <bereketb20@hotmail.com>
2023-01-20 15:05:55 +0300

    Type error

--- models/place.py
+++ models/place.py
@@ -11,10 +11,10 @@ from models.amenity import Amenity
 if models.storage_type == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
-                                 ForeignKey('place_id'), primary_key=True,
+                                 ForeignKey('places.id'), primary_key=True,
                                  nullable=False),
                           Column('amenity_id', String(60),
-                                 ForeignKey('amenities_id'), primary_key=True,
+                                 ForeignKey('amenities.id'), primary_key=True,
                                  nullable=False))
 
 
