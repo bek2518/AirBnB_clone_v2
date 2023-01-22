d9acff7a85b490e51099e164ba463a4e03fc009e
bek2518 <bereketb20@hotmail.com>
2023-01-22 12:36:57 +0300

    Blind trial

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Bash script that sets up webserver for deployment of web_static
-
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo service nginx start
@@ -9,9 +8,7 @@ sudo mkdir -p /data/web_static/shared
 echo "Fake HTML file with simple content to test Nginx configuration" > /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-
 CONFIG=\
 '\\t\tlocation /hbnb_static/ {\n\t\t\t\talias /data/web_static/current/; \n\t\t}\n'
 sudo sed -i "40i $CONFIG" /etc/nginx/sites-available/default
-
 sudo service nginx restart
