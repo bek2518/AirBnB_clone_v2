997b05ea72a9af33c52c176167c61c288b827ff9
bek2518 <bereketb20@hotmail.com>
2023-01-22 12:01:33 +0300

    Trial removed if and changed line

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 # Bash script that sets up webserver for deployment of web_static
 
-if [ ! -x /usr/sbin/nginx ]
-then
-	sudo apt -y update
-	sudo apt -y install nginx
-	sudo service nginx start
-fi
-
+sudo apt -y update
+sudo apt -y install nginx
+sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 echo "Fake HTML file with simple content to test Nginx configuration" > /data/web_static/release/test/index.html
@@ -16,6 +12,6 @@ sudo chown -R ubuntu:ubuntu /data
 
 CONFIG=\
 '\\t\tlocation /hbnb_static/ {\n\t\t\t\talias /data/web_static/current/; \n\t\t}\n'
-sudo sed -i "43i $CONFIG" /etc/nginx/sites-available/default
+sudo sed -i "40i $CONFIG" /etc/nginx/sites-available/default
 
 sudo service nginx restart
