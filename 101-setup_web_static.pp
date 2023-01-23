2795e5a5e177abc80549adecc6e79bb2d757acfb
bek2518 <bereketb20@hotmail.com>
2023-01-23 21:56:58 +0300

    Separated commands

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -4,32 +4,44 @@ exec { 'Installation':
   command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ;
     sudo service nginx start',
   provider => shell,
+  before   => ['Create first directory'],
 }
 
-exec { 'Create directories':
-  command  => 'sudo mkdir -p /data/web-static/releases/test ;
-    sudo mkdir -p /data/web-static/shared',
+exec { 'Create first directory':
+  command  => 'sudo mkdir -p /data/web-static/releases/test',
   provider => shell,
+  before   => ['Create second directory']
 }
 
-file  { 'Insert to file'
+exec { 'Create second directory':
+  command  =>'sudo mkdir -p /data/web-static/shared',
+  provider => shell,
+  before   => ['Insert to file'],
+}
+
+file  { 'Insert to file':
   esnure  => file,
   path    => '/data/web_static/releases/test/index.html',
   content => 'Fake HTML file with simple content to test Nginx configuration',
-  before  => Exec['link and owner']
+  before  => Exec['link']
 }
 
-exec { 'link and owner':
-  command  => 'sudo ln -fs /data/web_static/releases/test/ /data/web_static/current ;
-    sudo chown -R ubuntu:ubuntu /data',
+exec { 'link':
+  command  => 'sudo ln -fs /data/web_static/releases/test/ /data/web_static/current',
   provider => shell,
-  before  => Exec['Insert']
+  before   => Exec['Owner']
+}
+
+exec {'Owner':
+  command  => 'sudo chown -R ubuntu:ubuntu /data',
+  provider => shell,
+  before   => Exec['Insert'],
 }
 
 exec { 'Insert':
   command  =>'sudo sed -i "40i\\t\tlocation /hbnb_static/ {\n\t\t\t\talias /data/web_static/current/; \n\t\t}\n" /etc/nginx/sites-available/default',
   provider => shell,
-  before  => Exec['restart service']
+  before   => Exec['restart service'],
 }
 
 exec { 'restart service':
