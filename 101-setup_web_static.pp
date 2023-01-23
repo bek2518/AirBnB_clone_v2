143630aa4c3d87a2022c9a53584fe124cec4df8b
bek2518 <bereketb20@hotmail.com>
2023-01-23 21:14:57 +0300

    Separated commands

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,12 +1,35 @@
 # Bash script that sets up webserver for deployment of web_static
 
 exec { 'Installation':
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo service nginx start ;
-    sudo mkdir -p /data/web-static/releases/test ; sudo mkdir -p /data/web-static/shared ;
-    echo "Fake HTML file with simple content to test Nginx configuration" > /data/web_static/releases/test/index.html
-    ; sudo ln -fs /data/web_static/releases/test/ /data/web_static/current ;
-    sudo chown -R ubuntu:ubuntu /data ; 
-    sudo sed -i "40i\\t\tlocation /hbnb_static/ {\n\t\t\t\talias /data/web_static/current/; \n\t\t}\n" /etc/nginx/sites-available/default ;
-    sudo service nginx restart',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ;
+    sudo service nginx start',
   provider => shell,
 }
+
+exec { 'Create directories':
+  command  => 'sudo mkdir -p /data/web-static/releases/test ;
+    sudo mkdir -p /data/web-static/shared',
+  provider => shell,
+}
+
+file  { 'Insert to file'
+  esnure  => file,
+  path    => '/data/web_static/releases/test/index.html',
+  content => 'Fake HTML file with simple content to test Nginx configuration',
+}
+
+exec { 'link and owner':
+  command  => 'sudo ln -fs /data/web_static/releases/test/ /data/web_static/current ;
+    sudo chown -R ubuntu:ubuntu /data',
+  provider => shell,
+}
+
+exec { 'Insert':
+  command  =>'sudo sed -i "40i\\t\tlocation /hbnb_static/ {\n\t\t\t\talias /data/web_static/current/; \n\t\t}\n" /etc/nginx/sites-available/default',
+  provider => shell,
+}
+
+exec { 'restart service':
+  provider => shell,
+  command  => 'sudo service nginx restart'
+  }
