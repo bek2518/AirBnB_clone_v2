cb6ebb702d5871a0d4938845b013fd3828d64c8b
bek2518 <bereketb20@hotmail.com>
2023-01-25 16:02:20 +0300

    Shellcheck edit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,6 @@ sudo mkdir -p /data/web_static/shared
 echo "Fake HTML file with simple content to test Nginx configuration" > /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-CONFIG=\
-'\\t\tlocation /hbnb_static/ {\n\t\t\t\talias /data/web_static/current/; \n\t\t}\n'
+CONFIG='\\t\tlocation /hbnb_static/ {\n\t\t\t\talias /data/web_static/current/; \n\t\t}\n'
 sudo sed -i "40i $CONFIG" /etc/nginx/sites-available/default
 sudo service nginx restart
