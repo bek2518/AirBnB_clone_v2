bb57841aeff84487c5c8490d93a10bd3959cdd3c
bek2518 <bereketb20@hotmail.com>
2023-02-04 14:54:16 +0300

    Added python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''
+    Index that displays hello HBNB for / route
+    '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''
+    Index that displays HBNB for /hbnb route
+    '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    '''
+    Index that displays C followd by value of text for /c/<text> route
+    '''
+    return 'C %s' % escape(text).replace('_', ' ')
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    '''
+    Index that displays Python followd by value of text for
+    /python/<text> route
+    '''
+    return 'Python %s' % escape(text).replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
