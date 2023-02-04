8230b1f9fda6db84f6310725a2d49bd19f486ae7
bek2518 <bereketb20@hotmail.com>
2023-02-04 13:41:50 +0300

    Added listening ip and port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,7 +4,7 @@ Script that starts a Flask web application
 """
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes=False
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -13,3 +13,7 @@ def index():
     Index that displays hello HBNB when / used
     '''
     return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
