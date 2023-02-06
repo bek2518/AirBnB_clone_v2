c3e7d7c9d03ce59c9e0e0f6374138c7a533ade1e
bek2518 <bereketb20@hotmail.com>
2023-02-06 09:11:47 +0300

    Reverted back, changed HTML to upper case

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,15 +5,7 @@ Script that starts a Flask web application
 from flask import Flask, render_template
 from models import storage
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    '''
-    Displays HTML page with states
-    '''
-    states = storage.all("State")
-    return render_template('7-states_list.html', states=states)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -22,6 +14,15 @@ def teardown(exception):
     Removes SQL Alchemy session after each request
     '''
     storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    '''
+    Displays HTML page with states
+    '''
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
