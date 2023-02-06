5c3b05460a434b436e92ed0a311b50003c9ca20f
bek2518 <bereketb20@hotmail.com>
2023-02-06 09:31:38 +0300

    Trial

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,21 +8,17 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    '''
-    Removes SQL Alchemy session after each request
-    '''
-    storage.close()
-
-
 @app.route('/states_list')
 def states_list():
-    '''
-    Displays HTML page with states
-    '''
+    '''Displays HTML page with states'''
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    '''Removes SQL Alchemy session after each request'''
+    storage.close()
 
 
 if __name__ == "__main__":
