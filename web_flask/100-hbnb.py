57c54982e1890813d00f57ffa82017094adfda87
bek2518 <bereketb20@hotmail.com>
2023-02-07 09:23:03 +0300

    Pycodestyle edits

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -11,23 +11,22 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown(exception):
-	'''
-	Removes current SQLAlchemy session after each request
-	'''
-	storage.close()
+    '''
+    Removes current SQLAlchemy session after each request
+    '''
+    storage.close()
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	'''
-	Display index website with state and amenities
-	'''
-	states = storage.all('State')
-	amenities = storage.all('Amenity')
-	places = storage.all('Place')
-	return render_template('100-hbnb.html', states=states, 
-							amenities=amenities, places=places)
+    '''
+    Display index website with state and amenities
+    '''
+    st = storage.all('State')
+    am = storage.all('Amenity')
+    pl = storage.all('Place')
+    return render_template('100-hbnb.html', states=st, amenities=am, places=pl)
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
